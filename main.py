9eea269ee1f26f00a5bb34f8485628142b529aea
trwy7 <47907719+trwy7@users.noreply.github.com>
2024-10-06 15:54:01 -0600

    NTFY notification system

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from threading import Lock
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+import asyncio
 import typing
 import platform
 import shutil
@@ -23,6 +24,7 @@ from flask_wtf.csrf import CSRFProtect
 from flask_bcrypt import Bcrypt
 from markupsafe import escape
 import waitress
+import aiohttp
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.logger.info('Starting ClassFinder...')
 devmode = not platform.uname()[1] == 'classfinder'
@@ -33,6 +35,8 @@ smtp_server = os.environ.get('SMTP_SERVER', 'mail.smtp2go.com')
 smtp_port = int(os.environ.get('SMTP_PORT', 2525))
 smtp_user = os.environ.get('SMTP_USER', 'oneauth')
 smtp_password = os.environ.get('SMTP_PASSWORD', '')
+ntfy_url = os.environ.get('NTFY_URL', 'http://10.0.0.71:8957')
+ntfy_topic = os.environ.get('NTFY_TOPIC', 'classfinder')
 from_addr = os.environ.get('FROM_ADDR', 'classfinder@trey7658.com')
 canvas_url = os.environ.get('CANVAS_URL', 'https://stem.instructure.com/')
 limiter = Limiter(app=app, default_limits=["75/minute", "5/second"], headers_enabled=True, key_func=lambda: request.cookies.get('username') if 'username' in request.cookies else request.remote_addr, default_limits_exempt_when=lambda: request.cookies.get('username') in admins)
@@ -82,6 +86,25 @@ linkcodes = {}
 usermessages = {}
 emailids = {}
 adminmessages = [f"Server started at {datetime.datetime.now().strftime('%m %d, %Y %H:%M:%S')}"]
+
+async def send_ntfy(message: str, topic: str = ntfy_topic):
+    """Send a message to the ntfy server
+    message: The message to send
+    topic: The topic to send the message to"""
+    if 'pytest' in sys.modules:
+        app.logger.debug('NTFY bypassed')
+        print('NTFY bypassed by pytest')
+        return
+    if devmode:
+        app.logger.debug('NTFY bypassed in devmode')
+        return
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.post(f'{ntfy_url}/{topic}', data=message) as response:
+                if response.status != 200:
+                    app.logger.error('Failed to send message to ntfy server')
+    except requests.exceptions.ConnectionError:
+        app.logger.error('Could not connect to ntfy server')
 
 def backup(selection: typing.Literal['courses', 'users', 'requests', 'all'] = 'all', bypass: bool = False):
     """Backup the data to the json files
@@ -394,6 +417,7 @@ def signupwithid(emailid):
         response = jsonify({'status': 'success', 'message': 'Account created'})
         response.set_cookie('token', token, httponly=True, max_age=604800)
         response.set_cookie('username', username, httponly=True, max_age=604800)
+        asyncio.run(send_ntfy(f'New user created: {username} ({emailids[emailid]})'))
         backup('users')
         del emailids[emailid]
         return response
@@ -924,7 +948,12 @@ def admincreateaccount(username):
     password = request.json['password']
     if username in users:
         return jsonify({'status': 'failure', 'message': 'Username already exists'}), 400
-    token = createuser(username, password, createdby=requsername)
+    email = None
+    if 'email' in request.json:
+        email = request.json['email']
+        if not re.match(r'[a-z]*\.[a-z]*(@s.stemk12.org|@stemk12.org)', email):
+            return jsonify({'status': 'failure', 'message': 'Invalid email'}), 400
+    token = createuser(username, password, createdby=requsername, email=email)
     loginv = request.json['login']
     response = jsonify({'status': 'success', 'message': 'Account created', 'token': token})
     if loginv:
@@ -932,6 +961,7 @@ def admincreateaccount(username):
         response.set_cookie('username', username, httponly=True, max_age=604800)
         response.set_cookie('admtoken', request.cookies['token'], httponly=True, max_age=604800)
         response.set_cookie('admusername', request.cookies['username'], httponly=True, max_age=604800)
+    asyncio.run(send_ntfy(f'New user created by {requsername}: {username} ({str(email)})'))
     return response
 
 @app.route('/admin/deletecourse/', methods=['POST', 'GET'])
