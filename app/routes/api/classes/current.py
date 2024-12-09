126e5689cc97906fd71cd8b6675556018c613dae
trwy7 <47907719+trwy7@users.noreply.github.com>
2024-12-08 20:39:33 -0700

    Basic API endpoints

--- app/routes/api/classes/current.py
+++ app/routes/api/classes/current.py
@@ -4,3 +4,37 @@ from app.utilities.classes import get_user_current_period, get_today_courses
 from app.utilities.responses import success_response, error_response
 from datetime import datetime
 
+@app.route("/api/v2/classes/current")
+@verify_user
+def current_classes(user):
+    currentperiod = get_user_current_period(user)
+    return success_response(None, {
+        "classes": [
+            {
+                "name": c.name,
+                "room": c.room,
+                "period": c.period,
+                "lunch": c.lunch,
+                "verified": c.verified,
+                "canvasid": c.canvasid
+            } for c in get_today_courses(user)
+        ],
+        "currentperiod": currentperiod,
+        "endtime": int(datetime.combine(datetime.today(), currentperiod['end']).timestamp()) if (currentperiod is not None) else None,
+    })
+
+@app.route("/api/v2/classes/all")
+@verify_user
+def all_classes(user):
+    return success_response(None, {
+        "classes": [
+            {
+                "name": c.name,
+                "room": c.room,
+                "period": c.period,
+                "lunch": c.lunch,
+                "verified": c.verified,
+                "canvasid": c.canvasid
+            } for c in user.classes
+        ]
+    })
