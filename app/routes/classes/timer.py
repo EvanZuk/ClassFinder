dbd970c2a093fb30fd9f9986a127f8207ed43fce
trwy7 <47907719+trwy7@users.noreply.github.com>
2024-12-11 13:40:06 -0700

    FIX: Use != and not "is not"

--- app/routes/classes/timer.py
+++ app/routes/classes/timer.py
@@ -11,7 +11,7 @@ def timer(user):
     if user is None:
         period = get_current_period()
         if period is None:
-            if request.args.get('noredirect', "false") is not "false":
+            if request.args.get('noredirect', "false") != "false":
                 return render_template('timer.html', nextclass="nothing")
             return redirect(url_for('dashboard'))
         formatted_time = datetime.combine(datetime.now().date(), period['end']).strftime('%m/%d/%Y %I:%M %p')
@@ -19,7 +19,7 @@ def timer(user):
     else:
         period = get_user_current_period(user)
         if period is None:
-            if request.args.get('noredirect', "false") is not "false":
+            if request.args.get('noredirect', "false") != "false":
                 return render_template('timer.html', nextclass="nothing")
             return redirect(url_for('dashboard'))
         formatted_time = datetime.combine(datetime.now().date(), period['end']).strftime('%m/%d/%Y %I:%M %p')
