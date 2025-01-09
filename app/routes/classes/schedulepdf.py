c32e6e958ce5da58a22a97f28860a8f0b9b47b9f
trwy7 <47907719+trwy7@users.noreply.github.com>
2025-01-09 14:38:27 -0700

    Export settings menu

--- app/routes/classes/schedulepdf.py
+++ app/routes/classes/schedulepdf.py
@@ -1,7 +1,12 @@
 from app import app
 from app.utilities.times import create_schedule_pdf
 from app.utilities.users import verify_user
-from flask import request, send_file
+from flask import request, send_file, render_template
+
+@app.route("/classes/exportschedule", methods=["GET"])
+@verify_user(required=False)
+def exportpdf(user):
+    return render_template("pdf.html", user=user)
 
 @app.route("/classes/schedulepdf", methods=["GET"])
 @verify_user(required=False)
@@ -14,7 +19,7 @@ def schedulepdfday(user, days):
     if days == "all":
         ndays = "0,1,2,3,4,7,8"
     else:
-        ndays = days.lower().replace("monday", "0").replace("m", "0").replace("tuesday", "1").replace("t", "1").replace("wednesday", "2").replace("w", "2").replace("thursday", "3").replace("r", "3").replace("friday", "4").replace("f", "4").replace("eb", "7").replace("eg", "8")
+        ndays = days.lower().replace("monday", "0").replace("tuesday", "1").replace("wednesday", "2").replace("thursday", "3").replace("friday", "4").replace("eb", "7").replace("eg", "8")
     return send_file(create_schedule_pdf(
         user=user if 'nopersonal' not in request.args else None, 
         days=[int(day) for day in ndays.split(",")], 
