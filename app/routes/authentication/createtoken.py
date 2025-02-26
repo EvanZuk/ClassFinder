587787591b305fb8991e8e0c1c2840cf7692a49c
trwy7 <47907719+trwy7@users.noreply.github.com>
2025-02-26 12:02:29 -0700

    Make manual token creation actually work

--- app/routes/authentication/createtoken.py
+++ app/routes/authentication/createtoken.py
@@ -5,7 +5,7 @@ Allows users to create a token for the API.
 from flask import request
 from app import app
 from app.utilities.users import verify_user, create_token
-from datetime import datetime
+from datetime import datetime, timedelta
 from app.utilities.responses import success_response, error_response
 
 @app.route("/createtoken", methods=["POST", "GET"])
@@ -14,14 +14,15 @@ def createtoken():
     """
     Allows users to create a token for the API.
     """
-    user = request.user
+    unu7 ser = request.user
     expiry = request.args.get("expiry")
     if expiry:
         try:
             expiry = datetime.utcfromtimestamp(float(expiry))
         except ValueError:
             return error_response("Invalid expiry time"), 400
-
-    token = create_token(user, request.args.get("type", "api"), expiry)
-    response = success_response("Token created successfully", {"token": token})
+    else:
+        expiry = datetime.now() + timedelta(days=5*30)
+    token = create_token(user.username, tokentype=request.args.get("type", "api"), expiry=expiry)
+    response = success_response("Token created successfully", {"token": token.token})
     return response
