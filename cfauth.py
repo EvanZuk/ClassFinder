6f25a8c7567496f87f66051f3f82981bc106da46
trwy7 <47907719+trwy7@users.noreply.github.com>
2025-03-30 11:39:28 -0600

    Back to perfect lint score

--- cfauth.py
+++ cfauth.py
@@ -1,31 +1,43 @@
 """
 This module provides a way to get data from ClassFinder.
-This is not meant to be used with web applications, but rather for scripts that need to get data from ClassFinder, as this will run its own flask server to get credentials.
+Used for scripts that require authentication.
+Not to be used with web applications, as this runs its own flask server.
 """
 import threading
-from flask import Flask, request, jsonify
 import webbrowser
-import requests
 import time
 import logging
+from flask import Flask, request, jsonify
+import requests
 
 app = Flask(__name__)
-ntoken = None
+ntoken = None # pylint: disable=invalid-name
 
-def get_available_scopes():
-    req = requests.get("https://class.trey7658.com/api/v2/scopes")
+def get_available_scopes() -> dict:
+    """
+    Gets the available scopes from the ClassFinder API.
+    Returns a dictionary with keys being the scope names and values being the scope descriptions.
+    """
+    req = requests.get("https://class.trey7658.com/api/v2/scopes", timeout=5)
     if req.status_code == 200:
         return req.json()['scopes']
     logging.error("Failed to fetch scopes: %s - %s", req.status_code, req.text)
     return {}
 
-def get_token(scopes: list = [], openbrowser: bool=True, port: int = 5000) -> str:
-    global ntoken
+def get_token(scopes: list = [], openbrowser: bool=True, port: int = 5000) -> str: #pylint: disable=dangerous-default-value
+    """
+    Gets the token from ClassFinder.
+    """
+    global ntoken # pylint: disable=global-statement
     ntoken = None
     if openbrowser:
         webbrowser.open(f"https://class.trey7658.com/auth?redirect_url=http://localhost:{str(port)}/callback&scopes=" + ",".join(scopes))
     else:
-        logging.info("Please open the following URL in your browser: https://class.trey7658.com/auth?redirect_url=http://localhost:%s/callback&scopes=%s", str(port), ",".join(scopes))
+        logging.info(
+            "Please open the following URL in your browser: https://class.trey7658.com/auth?redirect_url=http://localhost:%s/callback&scopes=%s",
+            str(port),
+            ",".join(scopes)
+        )
     logging.info("Waiting for token...")
     server_thread = threading.Thread(target=run_server, args=(port,))
     server_thread.daemon = True
@@ -35,16 +47,20 @@ def get_token(scopes: list = [], openbrowser: bool=True, port: int = 5000) -> st
     return ntoken
 
 def run_server(port: int = 5000):
-    global ntoken
+    """
+    Runs the Flask server to get the token.
+    """
     logging.info("Starting callback server on port %s", port)
     app.run(port=port, debug=False, use_reloader=False)
 
 @app.route('/callback', methods=['GET'])
 def callback():
-    global ntoken
+    """
+    This route is called when the user is redirected back to the application after logging in.
+    """
+    global ntoken # pylint: disable=global-statement
     code = request.args.get('token')
     if code:
         ntoken = code
         return jsonify({"status": "success"}), 200
-    else:
-        return jsonify({"status": "error", "message": "No token received"}), 400
+    return jsonify({"status": "error", "message": "No token received"}), 400
